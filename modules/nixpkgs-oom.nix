a085dcb65eafd60a8f34aee1c9981bf274aa09e7
robert jakub <rj@project2.pl>
2025-11-18 00:33:46 +0100

    Update nixpkgs-oom.nix

--- modules/nixpkgs-oom.nix
+++ modules/nixpkgs-oom.nix
@@ -3,7 +3,7 @@
   nixpkgs.overlays = [
     (final: prev: {
       oom-hardware = import self.inputs.nixpkgs {
-        inherit (prev) system;
+        # inherit (prev) system;
         config = { inherit (prev.config) allowUnfree allowUnfreePredicate; };
         overlays = [ self.overlays.pkgs ];
       };
